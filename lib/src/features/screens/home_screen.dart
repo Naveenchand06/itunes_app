fb8d93a8a233de956883801a3c57a8f5bcef7c85
Naveen <naveenchand0606@gmail.com>
2024-07-24 22:36:47 +0530

    Grid View - Completed

--- lib/src/features/screens/home_screen.dart
+++ lib/src/features/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:itunes_app/src/constants/app_colors.dart';
+import 'package:itunes_app/src/constants/app_strings.dart';
 import 'package:itunes_app/src/features/models/search_response.dart';
 import 'package:itunes_app/src/features/repository/search_repository.dart';
 import 'package:itunes_app/src/features/screens/detail_screen.dart';
@@ -7,6 +9,7 @@ import 'package:itunes_app/src/network/models/app_response.dart';
 import 'package:itunes_app/src/utils/loading/loading_screen.dart';
 import 'package:itunes_app/src/widgets/app_error_widget.dart';
 import 'package:itunes_app/src/widgets/section_title.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({
@@ -34,10 +37,14 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
     super.initState();
   }
 
+  Size screenSize = const Size(0, 0);
+  bool _isGridView = true;
+
   @override
   Widget build(BuildContext context) {
-    // * Loading Listener
+    screenSize = MediaQuery.of(context).size;
 
+    // * Loading Listener
     ref.listen<AppResponse<SearchResponse>>(
       searchNotifierProvider,
       (previous, current) async {
@@ -83,85 +90,170 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
           final allCategory = searchValue.result!.categoryResults;
           return SingleChildScrollView(
             child: Column(
-              children: allCategory.map(
-                (category) {
-                  if (category.results.isNotEmpty) {
-                    // * Media type title
-                    return Column(
-                      children: [
-                        SectionTitle(title: category.category),
-                        // * List of Items for that Category
-                        ListView.builder(
-                          shrinkWrap: true,
-                          physics: NeverScrollableScrollPhysics(),
-                          itemCount: category.results.length,
-                          itemBuilder: (context, index) {
-                            final item = category.results[index];
-                            return InkWell(
-                              onTap: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) =>
-                                        DetailScreen(details: item)));
-                              },
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 20.0,
-                                  vertical: 12.0,
-                                ),
-                                child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisSize: MainAxisSize.max,
-                                  children: [
-                                    SizedBox(
-                                      height: 130.0,
-                                      width: 90.0,
-                                      child: Image.network(
-                                        item.artworkUrl100,
-                                        fit: BoxFit.cover,
+              children: [
+                ToggleSwitch(
+                  minWidth: screenSize.width,
+                  cornerRadius: 6.0,
+                  activeBgColors: const [
+                    [AppColors.field],
+                    [AppColors.field]
+                  ],
+                  inactiveBgColor: AppColors.tagField,
+                  changeOnTap: true,
+                  initialLabelIndex: _isGridView ? 0 : 1,
+                  totalSwitches: 2,
+                  labels: const [AppStrings.gridLayout, AppStrings.listLayout],
+                  radiusStyle: true,
+                  animate: false,
+                  onToggle: (index) {
+                    if (index == 0) {
+                      setState(() {
+                        _isGridView = true;
+                      });
+                    } else {
+                      setState(() {
+                        _isGridView = false;
+                      });
+                    }
+                  },
+                ),
+                const SizedBox(height: 10.0),
+                SingleChildScrollView(
+                  child: Column(
+                    children: allCategory.map(
+                      (category) {
+                        if (category.results.isNotEmpty) {
+                          // * Media type title
+                          return Column(
+                            children: [
+                              SectionTitle(title: category.category),
+                              if (_isGridView)
+                                // * Grid of Items for that category
+                                Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: GridView(
+                                      physics:
+                                          const NeverScrollableScrollPhysics(),
+                                      shrinkWrap: true,
+                                      gridDelegate:
+                                          const SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 3,
+                                        mainAxisSpacing: 30.0,
+                                        crossAxisSpacing: 8.0,
                                       ),
-                                    ),
-                                    const SizedBox(width: 12.0),
-                                    Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Wrap(
-                                          children: [
-                                            SizedBox(
-                                              width: 250.0,
-                                              child: Text(item.trackName,
+                                      children: category.results.map((item) {
+                                        return InkWell(
+                                          onTap: () {
+                                            Navigator.of(context).push(
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        DetailScreen(
+                                                            details: item)));
+                                          },
+                                          child: Column(
+                                            children: [
+                                              SizedBox(
+                                                height: 120.0,
+                                                width: 120.0,
+                                                child: Image.network(
+                                                  item.artworkUrl100,
+                                                  fit: BoxFit.cover,
+                                                ),
+                                              ),
+                                              const SizedBox(height: 4.0),
+                                              Text(item.trackName,
                                                   overflow:
                                                       TextOverflow.ellipsis,
-                                                  maxLines: 2,
+                                                  maxLines: 1,
                                                   style: Theme.of(context)
                                                       .textTheme
                                                       .bodyMedium),
+                                            ],
+                                          ),
+                                        );
+                                      }).toList()),
+                                ),
+                              if (!_isGridView)
+                                // * List of Items for that Category
+                                ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  itemCount: category.results.length,
+                                  itemBuilder: (context, index) {
+                                    final item = category.results[index];
+                                    return InkWell(
+                                      onTap: () {
+                                        Navigator.of(context).push(
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    DetailScreen(
+                                                        details: item)));
+                                      },
+                                      child: Container(
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 20.0,
+                                          vertical: 12.0,
+                                        ),
+                                        child: Row(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisSize: MainAxisSize.max,
+                                          children: [
+                                            SizedBox(
+                                              height: 130.0,
+                                              width: 90.0,
+                                              child: Image.network(
+                                                item.artworkUrl100,
+                                                fit: BoxFit.cover,
+                                              ),
                                             ),
+                                            const SizedBox(width: 12.0),
+                                            Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Wrap(
+                                                  children: [
+                                                    SizedBox(
+                                                      width: 250.0,
+                                                      child: Text(
+                                                          item.trackName,
+                                                          overflow: TextOverflow
+                                                              .ellipsis,
+                                                          maxLines: 2,
+                                                          style:
+                                                              Theme.of(context)
+                                                                  .textTheme
+                                                                  .bodyMedium),
+                                                    ),
+                                                  ],
+                                                ),
+                                                const SizedBox(height: 4.0),
+                                                Text(item.artistName,
+                                                    overflow: TextOverflow.clip,
+                                                    style: Theme.of(context)
+                                                        .textTheme
+                                                        .bodyMedium),
+                                              ],
+                                            )
                                           ],
                                         ),
-                                        const SizedBox(height: 4.0),
-                                        Text(item.artistName,
-                                            overflow: TextOverflow.clip,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .bodyMedium),
-                                      ],
-                                    )
-                                  ],
+                                      ),
+                                    );
+                                  },
                                 ),
-                              ),
-                            );
-                          },
-                        ),
-                      ],
-                    );
-                  } else {
-                    return const SizedBox.shrink();
-                  }
-                },
-              ).toList(),
+                            ],
+                          );
+                        } else {
+                          return const SizedBox.shrink();
+                        }
+                      },
+                    ).toList(),
+                  ),
+                ),
+              ],
             ),
           );
         } else {
